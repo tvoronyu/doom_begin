c292935704740ac2d5d1661696bc3d0acddb62ed
tvoronyu <tipok777martin@gmail.com>
2019-02-17 21:19:46 +0200

    doom v1.1

---
+++ cmake-build-debug/CMakeFiles/WOOLF.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WOOLF.dir/src/main.c.o"
+  "WOOLF.pdb"
+  "WOOLF"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/WOOLF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
